86bd67021a9790ccbbe9388d5acaa591bd15cf65
John Alexis Morandarte <aal@Johns-Air.attlocal.net>
2019-01-25 16:24:35 -0600

    Assignment 3 completed

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,7 +1,44 @@
 const inquirer = require("inquirer");
+const Contact = require("../db/models").Contact;
+
 
 module.exports = class ContactController {
 
-  constructor(){}
+  constructor(){
+      this.contacts =[];
+      this.addContactQuestions = [
+        {
+          type: "input",
+          name: "name",
+          message: "Contact's name - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+        {
+          type: "input",
+          name: "phone",
+          message: "Contact's phone number - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+
+        {
+          type: "input",
+          name: "Email",
+          message: "Contact's email - ",
+          validate(val){
+            return val !== "";
+          }
+        },  
+
+
+      ];
+  }
+
+  addContact(name, phone, Email){
+    return Contact.create({name, phone, Email})
+  }
 
 }
--- db/models/contact.js
+++ db/models/contact.js
@@ -2,10 +2,9 @@
 module.exports = (sequelize, DataTypes) => {
   var Contact = sequelize.define('Contact', {
     name: DataTypes.STRING,
-    phone: DataTypes.STRING
+    phone: DataTypes.STRING,
+    Email: DataTypes.STRING,
   }, {});
-  Contact.associate = function(models) {
-    // associations can be defined here
-  };
+  Contact.associate = function(models) {};
   return Contact;
 };
